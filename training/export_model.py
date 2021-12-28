403c04e5ff85569bf00f5b13129564b77708f989
Constantin Pape <constantin.pape@embl.de>
2021-12-28 16:50:15 +0100

    Implement model export

---
+++ training/export_model.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+import h5py
+from torch_em.util import export_bioimageio_model, get_default_citations
+
+
+def export_to_bioimageio(version):
+    path = "./data/sampleA.h5"
+    with h5py.File(path, "r") as f:
+        input_data = f["volumes/raw"][:16, :256, :256]
+
+    checkpoint = f"./checkpoints/cremi3d-v{version}"
+
+    name = f"cremi-v{version}"
+    description = "Affinity segmentation"
+    tags = ["u-net", "cell-segmentation", "segmentation", "phase-contrast", "livecell"]
+
+    # eventually we should refactor the citation logic
+    cite = get_default_citations(model="UNet2d")
+
+    output = f"./networks/{name}"
+    os.makedirs(output, exist_ok=True)
+
+    doc = "my doc"
+    export_bioimageio_model(
+        checkpoint, output,
+        input_data=input_data,
+        name=name,
+        description=description,
+        authors=[{"name": "Constantin Pape", "affiliation": "EMBL Heidelberg"}],
+        tags=tags,
+        license="CC-BY-4.0",
+        documentation=doc,
+        git_repo=None,
+        cite=cite,
+        input_optional_parameters=False,
+        maintainers=[{"github_user": "constantinpape"}],
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", required=True, type=int)
+    args = parser.parse_args()
+    export_to_bioimageio(args.version)
