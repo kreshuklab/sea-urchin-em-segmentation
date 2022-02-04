6af69d3d3c0f68922c94565b5740b635b94781fb
Constantin Pape <c.pape@gmx.net>
2022-02-04 21:56:07 +0100

    Update segment_test_volume

--- segmentation/segment_test_volume.py
+++ segmentation/segment_test_volume.py
@@ -12,27 +12,30 @@ from ilastik.experimental.api import from_project_file
 from xarray import DataArray
 
 
-def predict_rf(raw, path, ilp, force=False):
-    with h5py.File(path, "r") as f:
-        if "rf_pred" in f and not force:
-            print("Load random forest prediction ...")
-            return f["rf_pred"][:]
+def predict_rf(raw, path, ilp, prefix, force=False):
+    key = f"{prefix}/rf_pred"
+    if os.path.exists(path):
+        with h5py.File(path, "r") as f:
+            if key in f and not force:
+                print("Load random forest prediction ...")
+                return f[key][:]
     print("Run random forest prediction ...")
     ilp = from_project_file(ilp)
     input_ = DataArray(raw, dims=("z", "y", "x"))
     pred = ilp.predict(input_).values[..., 1]
     with h5py.File(path, "a") as f:
-        ds = f.require_dataset("rf_pred", shape=pred.shape, compression="gzip", dtype=pred.dtype)
+        ds = f.require_dataset(key, shape=pred.shape, compression="gzip", dtype=pred.dtype)
         ds[:] = pred
     return pred
 
 
-def predict_enhancer(rf_pred, path, model, force=False):
+def predict_enhancer(rf_pred, path, model, prefix, force=False):
+    key = f"{prefix}/enhancer_pred"
     model = bioimageio.core.load_resource_description(model)
     with h5py.File(path, "r") as f:
-        if "enhancer_pred" in f and not force:
+        if key in f and not force:
             print("Load prediction from memory ...")
-            return f["enhancer_pred"][:]
+            return f[key][:]
     print("Run enhancer prediction ...")
     tiling = {"halo": {"x": 16, "y": 16, "z": 4},
               "tile": {"x": 224, "y": 224, "z": 12}}
@@ -40,16 +43,17 @@ def predict_enhancer(rf_pred, path, model, force=False):
         inputs = [DataArray(rf_pred[None, None], dims=tuple(pp.input_specs[0].axes))]
         pred = predict_with_tiling(pp, inputs, tiling, verbose=True)[0].values.squeeze()
     with h5py.File(path, "a") as f:
-        ds = f.require_dataset("enhancer_pred", shape=pred.shape, compression="gzip", dtype=pred.dtype)
+        ds = f.require_dataset(key, shape=pred.shape, compression="gzip", dtype=pred.dtype)
         ds[:] = pred
     return pred
 
 
-def segment(pred, path, force=False, n_threads=8):
+def segment(pred, path, prefix, force=False, n_threads=8):
+    key = f"{prefix}/segmentations"
     with h5py.File(path, "r") as f:
-        if "segmentation" in f and not force:
+        if key in f and not force:
             print("Load segmentation from memory ...")
-            return f["segmentation"][:]
+            return f[key][:]
     print("Run multicut segmentation ...")
     ws, max_id = eseg.stacked_watershed(pred, n_threads=n_threads, threshold=0.3, sigma_seeds=2.0)
     rag = eseg.compute_rag(ws, max_id + 1, n_threads)
@@ -64,7 +68,7 @@ def segment(pred, path, force=False, n_threads=8):
     node_labels = eseg.multicut.multicut_decomposition(rag, costs, n_threads=n_threads)
     seg = eseg.project_node_labels_to_pixels(rag, node_labels, n_threads)
     with h5py.File(path, "a") as f:
-        ds = f.require_dataset("segmentation", shape=seg.shape, compression="gzip", dtype=seg.dtype)
+        ds = f.require_dataset(key, shape=seg.shape, compression="gzip", dtype=seg.dtype)
         ds[:] = seg
     return seg
 
@@ -80,30 +84,32 @@ def view_segmentation(raw, rf_pred, pred, seg):
     napari.run()
 
 
-def segment_test_volume(path, ilp, model, force, view):
+def segment_test_volume(path, out_path, ilp, model, force, view):
     with h5py.File(path, "r") as f:
         raw = f["raw"][:]
-    rf_pred = predict_rf(raw, path, ilp, force)
-    pred = predict_enhancer(rf_pred, path, model, force)
-    seg = segment(pred, path, force)
+    prefix = os.path.splitext(os.path.basename(ilp))[0]
+    rf_pred = predict_rf(raw, out_path, ilp, prefix, force)
+    pred = predict_enhancer(rf_pred, out_path, model, prefix, force)
+    seg = segment(pred, out_path, prefix, force)
     if view:
         view_segmentation(raw, rf_pred, pred, seg)
 
 
 def main():
     this_folder = os.path.split(__file__)[0]
-    default_ilp = os.path.join(this_folder, "../ilastik_projects/boundary_prediction_v2.ilp")
     default_model = os.path.join(this_folder, "../networks/cremi-v1.zip")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--vol_id", type=int, required=True)
-    parser.add_argument("-i", "--ilp", default=default_ilp)
+    parser.add_argument("-i", "--ilp", required=True)
     parser.add_argument("-m", "--model", default=default_model)
     parser.add_argument("-f", "--force", type=int, default=0)
     parser.add_argument("--view", type=int, default=0)
     args = parser.parse_args()
     path = os.path.join(this_folder, f"../test_volumes/vol{args.vol_id}.h5")
-    segment_test_volume(path, args.ilp, args.model, bool(args.force), bool(args.view))
+    os.makedirs("data", exist_ok=True)
+    out_path = f"./data/vol{args.vol_id}.h5"
+    segment_test_volume(path, out_path, args.ilp, args.model, bool(args.force), bool(args.view))
 
 
 if __name__ == "__main__":
